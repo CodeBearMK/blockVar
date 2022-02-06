f29f089d2991d9c68460546f0654aa9431ad3b36
cat32tw <cat32tw@SWEETLIFE>
2022-02-06 12:11:27 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace blockVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int n = 0; n < 6; n++)
+            {
+                Console.WriteLine(" n = {0} ", n);
+            }
+            Console.WriteLine(" n = {0} ", n);
+            Console.Read();
+        }
+    }
+}
